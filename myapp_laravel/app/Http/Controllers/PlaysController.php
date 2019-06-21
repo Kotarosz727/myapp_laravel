d2e069f65a9c4de24f7f56933750dc67edce2359
Kotaro Suzuki <kotarosz727@gmail.com>
2019-06-22 08:35:06 +0900

    create PlaysController

---
+++ myapp_laravel/app/Http/Controllers/PlaysController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlaysController extends Controller
+{
+    function index(){
+        return "Hello World";
+    }
+}
--- myapp_laravel/public/index.php
+++ myapp_laravel/public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-echo "hello";
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
